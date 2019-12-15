c5faa2732efe87170e738982abbb984d956d652f
Kamrul Hasan <mkhrussell@gmail.com>
2019-12-15 16:27:13 +0600

    Updated UI code

--- SwiftRestClientDemo/Models/User.swift
+++ SwiftRestClientDemo/Models/User.swift
@@ -66,10 +66,15 @@ struct Company: Codable {
 struct User: Codable {
     let id: Int
     let name: String
-    let username: String
+    let nickname: String
     let email: String
     let address: Address
     let phone: String
     let website: String
     let company: Company
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, email, address, phone, website, company
+        case nickname = "username"
+    }
 }
--- SwiftRestClientDemo/ViewController.swift
+++ SwiftRestClientDemo/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tvUserInfo: UITextView!
+
+    var user: User?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        loadData()
     }
 
+    private func loadData() {
+        let client = TypicodeClient()
+        client.fetchUser(id: 1) { (user) in
+            if let user = user {
+                DispatchQueue.main.async {
+                    self.user = user
+                    self.updateUI()
+                }
+            }
+        }
+    }
+
+    private func updateUI() {
+        guard let user = self.user else { return }
+
+        let userInfo = """
+            Id: \(user.id)
+            Name: " \(user.name)
+            Nickname: \(user.nickname)
+            Email: \(user.email)
+            Address:
+                Street: \(user.address.street)
+                Suite: \(user.address.suite)
+                City: \(user.address.city)
+                Zipcode: \(user.address.zipcode)
+                Geo-location:
+                    lat: \(user.address.geo.lat)
+                    long: \(user.address.geo.long)
+            Phone: \(user.phone)
+            Website: \(user.website)
+            Company:
+                Name: \(user.company.name)
+                Catch Phrase: \(user.company.catchPhrase)
+                Business: \(user.company.business)
+            """
+
+        tvUserInfo.text = userInfo
+    }
 
 }
 
